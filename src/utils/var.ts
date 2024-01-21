eed1e29667f7da2609ece6f2ed1801cd0a4646b5
Simon Wessendorf <simon@wessendorf.software>
2024-01-21 16:23:43 +0100

    Usage of .env for variable replacements

--- src/utils/var.ts
+++ src/utils/var.ts
@@ -1,49 +1,67 @@
-import { varMap } from "../varMap";
-
-export interface Var {
-    text: string[] | string,
-    link?: string
-}
-
-export interface VarMap {
-    [name: string]: Var
-}
+const envPrefix = 'VITE_VAR_';
 
 const loremIpsum = [
     'lorem', 'ipsum', 'dolor', 'sit', 'amet'
 ];
 let loremIpsumIdx = 0;
 
+const delimiter: string = import.meta.env[envPrefix + 'DELIMITER'];
+const replacements: string = import.meta.env[envPrefix + 'REPLACE'];
+
 export function setupVar(el: HTMLElement) {
     const varName = el.dataset['var'];
     if (varName === undefined)
-      throw new Error('Expected dataset.var to be defined');
+      throw new Error('Expected "dataset.var" to be defined.');
 
-    const variable = varMap[varName];
-    if (variable === undefined) {
-      console.error(`Undefined var "${varName}"`);
+    const envKey = envPrefix + varName.toUpperCase();
+    const value: string|undefined = import.meta.env[envKey];
+    if (value === undefined) {
+      console.error(`Expected the environment variable "${envKey}" to be set.`);
       return;
     }
 
-    if (variable.link !== undefined)
-        el.addEventListener('click', () => window.open(variable.link));
-    
-    if (typeof variable.text === 'string') {
-        el.innerText = variable.text;
-        return;
+    let replaced = value;
+    let toReplace = null;
+    for (const char of replacements) {
+        if (toReplace === null) {
+            toReplace = char;
+            continue;
+        }
+
+        replaced = replaced.replace(toReplace, char);
+        toReplace = null;
     }
 
-    for (const part of variable.text) {
+    const split = replaced.split(delimiter);
+
+    const linkPrefixEnvKey = envKey + '_LINK_PREFIX';
+    const linkPrefix: string|undefined = import.meta.env[linkPrefixEnvKey];
+    if (linkPrefix !== undefined)
+        el.addEventListener('click', () => window.open(linkPrefix + split.join('')));
+
+    const labelSeparatorEnvKey = envKey + '_LABEL_SEPARATOR';
+    const labelSeparator = import.meta.env[labelSeparatorEnvKey] ?? '';
+
+    while (el.firstChild)
+        el.firstChild.remove();
+
+    let isFirst = true;
+
+    for (const part of split) {
+        if (!isFirst) {
+            const noPartEl = document.createElement('span');
+            noPartEl.classList.add('no-part');
+            noPartEl.innerText = loremIpsum[loremIpsumIdx];
+            el.appendChild(noPartEl);
+
+            loremIpsumIdx = (loremIpsumIdx + 1) % loremIpsum.length;
+        }
+
         const partEl = document.createElement('span');
         partEl.classList.add('part');
-        partEl.innerText = part;
+        partEl.innerText = isFirst ? part : labelSeparator + part;
         el.appendChild(partEl);
 
-        const noPartEl = document.createElement('span');
-        noPartEl.classList.add('no-part');
-        noPartEl.innerText = loremIpsum[loremIpsumIdx];
-        el.appendChild(noPartEl);
-
-        loremIpsumIdx = (loremIpsumIdx + 1) % loremIpsum.length;
+        isFirst = false;
     }
 }
