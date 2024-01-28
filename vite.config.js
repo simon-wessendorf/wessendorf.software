7deb50ea443cd1660890d1a237139346491a83ec
Simon Wessendorf <simon@wessendorf.software>
2024-01-28 14:03:02 +0100

    Moved sitemap

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, '/sitemap/index.html'),
+
         germanContact: resolve(__dirname, '/de/kontakt/index.html'),
+        germanNotice: resolve(__dirname, '/de/impressum/index.html'),
         germanPrivacy: resolve(__dirname, '/de/datenschutzerklärung/index.html'),
+        germanNotFound: resolve(__dirname, '/de/404.html'),
+
+        englishContact: resolve(__dirname, '/en/contact/index.html'),
+        englishNotice: resolve(__dirname, '/en/site-notice/index.html'),
+        englishPrivacy: resolve(__dirname, '/en/privacy-policy/index.html'),
+        englishNotFound: resolve(__dirname, '/en/404.html'),
       },
     },
   },
